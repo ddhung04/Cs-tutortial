d69df3b0cc9f9b1dfe15a70f83465f2a933ad291
ddhung04 <hungwibu2004@gmail.com>
2024-01-22 10:04:40 +0700

    push

--- DaHinh/Program.cs
+++ DaHinh/Program.cs
@@ -10,10 +10,17 @@ namespace DaHinh
     {
         static void Main(string[] args)
         {
+            Rectangle rectangle = new Rectangle(5.0, 7.0, "Red", true);
+            Console.WriteLine(rectangle.ToString());
+            Console.ReadLine();
             Square square = new Square(5.0, "Red", true);
             Console.WriteLine(square.ToString());
             square.SetSide(7.0);
             Console.WriteLine(square.ToString());
+            double area = square.GetArea();
+            double perimeter = square.GetPerimeter();
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Perimeter: {perimeter}");
             Console.ReadKey();
         }
     }

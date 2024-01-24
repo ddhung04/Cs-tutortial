b84338bff8e7da7d2c21811fe3dd92b93d37dbab
ddhung04 <hungwibu2004@gmail.com>
2024-01-24 13:50:09 +0700

    push

---
+++ ISP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public interface ISpeedDisplay
+    {
+        void DisplaySpeed();
+    }
+
+    public class Car : IMovable, ISpeedDisplay
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving...");
+        }
+
+        public void DisplaySpeed()
+        {
+            Console.WriteLine("Car speed is displayed...");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // do something
+        }
+    }
+}

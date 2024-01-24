9d0320a9990b0e306784e23bce9ffefa342fecbe
ddhung04 <hungwibu2004@gmail.com>
2024-01-24 13:13:59 +0700

    push

---
+++ Qly_danhba/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qly_danhba
+{
+    // Class đại diện cho một liên lạc
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public Contact(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+    }
+
+    // Interface định nghĩa các phương thức liên quan đến quản lý danh bạ
+    public interface IContactRepository
+    {
+        void AddContact(Contact contact);
+        void RemoveContact(Contact contact);
+        List<Contact> GetAllContacts();
+    }
+
+    // Class quản lý lưu trữ danh bạ
+    public class ContactRepository : IContactRepository
+    {
+        private List<Contact> contacts;
+
+        public ContactRepository()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void RemoveContact(Contact contact)
+        {
+            contacts.Remove(contact);
+        }
+
+        public List<Contact> GetAllContacts()
+        {
+            return contacts;
+        }
+    }
+
+    // Class quản lý hiển thị danh bạ
+    public class ContactViewer
+    {
+        private IContactRepository contactRepository;
+
+        public ContactViewer(IContactRepository repository)
+        {
+            contactRepository = repository;
+        }
+
+        public void DisplayContacts()
+        {
+            List<Contact> contacts = contactRepository.GetAllContacts();
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"Name: {contact.Name}, Phone: {contact.Phone}");
+            }
+        }
+    }
+
+    // Class chính
+    class Program
+    {
+        static void Main()
+        {
+            IContactRepository contactRepository = new ContactRepository();
+            ContactViewer contactViewer = new ContactViewer(contactRepository);
+
+            // Sử dụng các chức năng của ContactRepository và ContactViewer
+            Contact newContact = new Contact("John Doe", "123-456-7890");
+            contactRepository.AddContact(newContact);
+            contactViewer.DisplayContacts();
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Xuly_donhang/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xuly_donhang
+{
+    public class Order
+    {
+        public static readonly List<Order> OrderList = new List<Order>();
+        public string NameProduct { get; set; }
+        public string Description { get; set; }
+        public int OrderId { get; set; } = 0;
+        public int Cost { get; set; }
+        public IPaymentMethod PaymentMethods { get; set; }
+        public string OrderStatus = "Unpaid";
+
+        public Order(string NameProduct, string Description, int OrderId, int Cost, IPaymentMethod PaymentMethod)
+        {
+            this.NameProduct = NameProduct;
+            this.Description = Description;
+            this.OrderId = OrderId;
+            this.Cost = Cost;
+            this.PaymentMethods = PaymentMethod;
+            OrderList.Add(this); // lưu vào List ngay khi khởi tạo
+        }
+
+        public static List<Order> GetOrderList()
+        {
+            return OrderList;
+        }
+    }
+
+    // Các phương thức thanh toán _ có thể mở rộng _
+    public interface IPaymentMethod
+    {
+        void ProcessPayment(Order order);
+    }
+    public class CreditCardPayment : IPaymentMethod
+    {
+        public void ProcessPayment(Order order)
+        {
+            Console.WriteLine($"The order {order.OrderId} has been paid by Credit Card");
+        }
+    }
+    public class PayPalPayment : IPaymentMethod
+    {
+        public void ProcessPayment(Order order)
+        {
+            Console.WriteLine($"The order {order.OrderId} has been paid by PayPal");
+
+        }
+    }
+    // mở rộng nếu cần
+
+    public class OrderProcessor
+    {
+        public void ProcessOrder(Order order)
+        {
+                order.PaymentMethods?.ProcessPayment(order);
+                order.OrderStatus = "Paid";
+        }
+    }
+
+    internal class Program
+    {
+        // show ra list order
+        static void ShowOrder(List<Order> orderlist)
+        {
+            foreach(var order in orderlist)
+            {
+                Console.WriteLine($"Name: {order.NameProduct}, Description: {order.Description}, OrderId: {order.OrderId}, Cost: {order.Cost}, PaymentMethod: {order.PaymentMethods.GetType().Name}, Status: {order.OrderStatus}");
+            }
+        }
+
+        // xử lý thanh toán đơn hàng
+        static void Process(Order order)
+        {
+            OrderProcessor orderProcessor = new OrderProcessor();
+            orderProcessor.ProcessOrder(order);
+        }
+
+        static void Main(string[] args)
+        {
+            Order myOrder = new Order("IP15", "Fake", 2512, 2000000, new CreditCardPayment());
+            List<Order> orderList = Order.GetOrderList();
+
+            ShowOrder(orderList);
+
+            Process(myOrder);
+
+            ShowOrder(orderList);
+
+            Console.ReadKey();
+        }
+    }
+}
